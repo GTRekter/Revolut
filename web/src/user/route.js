e3bc94f8c5445a327748a2cb3afd0606434819f6
Ivan (이반) Porta <porta.ivan@outlook.com>
2023-05-14 20:07:24 +0200

    Update route.js

--- web/src/user/route.js
+++ web/src/user/route.js
@@ -5,6 +5,7 @@ const controller = require('./controller');
 const model = require('./model');
 
 router.put('/hello/:username', async (req, res) => {
+    console.log(`PUT /hello/:username begin`)
     try {
         const username = req.params.username;
         console.log(`username: ${username}`)
@@ -16,29 +17,38 @@ router.put('/hello/:username', async (req, res) => {
         }
         if (!controller.isValidDateOfBirth(dateOfBirth)) {
             console.log(`Invalid dateOfBirth`)
-          return res.status(400).send(`The provided date of birth (${dateOfBirth}) is invalid. Please insert a date in the format YYYY-MM-DD that is before today.`);
+            return res.status(400).send(`The provided date of birth (${dateOfBirth}) is invalid. Please insert a date in the format YYYY-MM-DD that is before today.`);
         }
         const user = {
             username: username,
             dateOfBirth: dateOfBirth
         }; 
         await model.saveUser(user);
+        console.log(`PUT /hello/:username end`)
         return res.sendStatus(204);
     } catch (error) {
+        console.log(`PUT /hello/:username end`)
         return res.status(500).send(error.message);
     }    
 });
 router.get('/hello/:username', async (req, res) => {
+    console.log(`GET /hello/:username begin`)
     try {
-        const username = req.params.username;    
+        const username = req.params.username;   
+        console.log(`username: ${username}`)
         const user = await model.getUserByUsername(username);
+        console.log(`user: ${user}`)
         if (!user) {
+            console.log(`GET /hello/:username end`)
             return res.status(404).send('User not found');
         }
         const message = controller.getBirthdayMessage(username, user.dateOfBirth);
+        console.log(`message: ${message}`)
+        console.log(`GET /hello/:username end`)
         return res.status(200).send(message);
     } catch (error) {
-      return res.status(500).send(error.message);
+        console.log(`GET /hello/:username end`)
+        return res.status(500).send(error.message);
     }
 });
 router.get('/', (req, res) => {
