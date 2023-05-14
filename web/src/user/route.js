0af3228c1853f8030ceb185b0756789c653352aa
Ivan (이반) Porta <porta.ivan@outlook.com>
2023-05-14 19:27:46 +0200

    Update route.js

--- web/src/user/route.js
+++ web/src/user/route.js
@@ -5,19 +5,19 @@ const controller = require('./controller');
 const model = require('./model');
 
 router.put('/hello/:username', async (req, res) => {
-    const username = req.params.username;
-    const dateOfBirth = req.body.dateOfBirth;
-    if (!controller.isValidUsername(username)) {
-        return res.status(400).send(`The provided username (${username}) is invalid. Please provide a username consisting of letters only.`);
-    }
-    if (!controller.isValidDateOfBirth(dateOfBirth)) {
-      return res.status(400).send(`The provided date of birth (${dateOfBirth}) is invalid. Please insert a date in the format YYYY-MM-DD that is before today.`);
-    }
-    const user = {
-        username: username,
-        dateOfBirth: dateOfBirth
-    };
     try {
+        const username = req.params.username;
+        const dateOfBirth = req.body.dateOfBirth;
+        if (!controller.isValidUsername(username)) {
+            return res.status(400).send(`The provided username (${username}) is invalid. Please provide a username consisting of letters only.`);
+        }
+        if (!controller.isValidDateOfBirth(dateOfBirth)) {
+          return res.status(400).send(`The provided date of birth (${dateOfBirth}) is invalid. Please insert a date in the format YYYY-MM-DD that is before today.`);
+        }
+        const user = {
+            username: username,
+            dateOfBirth: dateOfBirth
+        }; 
         await model.saveUser(user);
         return res.sendStatus(204);
     } catch (error) {
@@ -25,14 +25,14 @@ router.put('/hello/:username', async (req, res) => {
     }    
 });
 router.get('/hello/:username', async (req, res) => {
-    const username = req.params.username;
     try {
-      const user = await model.getUserByUsername(username);
-      if (!user) {
-        return res.status(404).send('User not found');
-      }
-      const message = controller.getBirthdayMessage(username, user.dateOfBirth);
-      return res.status(200).send(message);
+        const username = req.params.username;    
+        const user = await model.getUserByUsername(username);
+        if (!user) {
+            return res.status(404).send('User not found');
+        }
+        const message = controller.getBirthdayMessage(username, user.dateOfBirth);
+        return res.status(200).send(message);
     } catch (error) {
       return res.status(500).send(error.message);
     }
