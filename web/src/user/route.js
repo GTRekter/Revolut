34293f9831f627ce0f7811d90aa7199a46e9a83b
Ivan (이반) Porta <porta.ivan@outlook.com>
2023-05-14 20:01:42 +0200

    Update route.js

--- web/src/user/route.js
+++ web/src/user/route.js
@@ -7,11 +7,15 @@ const model = require('./model');
 router.put('/hello/:username', async (req, res) => {
     try {
         const username = req.params.username;
+        console.log(`username: ${username}`)
         const dateOfBirth = req.body.dateOfBirth;
+        console.log(`dateOfBirth: ${dateOfBirth}`)
         if (!controller.isValidUsername(username)) {
+            console.log(`Invalid username`)
             return res.status(400).send(`The provided username (${username}) is invalid. Please provide a username consisting of letters only.`);
         }
         if (!controller.isValidDateOfBirth(dateOfBirth)) {
+            console.log(`Invalid dateOfBirth`)
           return res.status(400).send(`The provided date of birth (${dateOfBirth}) is invalid. Please insert a date in the format YYYY-MM-DD that is before today.`);
         }
         const user = {
