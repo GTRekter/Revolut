434aa1721e64766109e4cd7dad9adefd232139d8
Ivan (이반) Porta <porta.ivan@outlook.com>
2023-05-14 20:05:39 +0200

    Update datastore.js

--- web/src/data/datastore.js
+++ web/src/data/datastore.js
@@ -3,15 +3,19 @@ require('dotenv').config();
 
 let datastore;
 if (process.env.GCP_USE_EMULATOR === 'true') {
+  console.log(`Initializing Datastore`)
   datastore = new Datastore({
     projectId: process.env.GCP_PROJECT_ID,
     apiEndpoint: process.env.DATASTORE_HOST,
   });
+  console.log(`Datastore initialized`)
 } else {
+  console.log(`Initializing Datastore`)
   datastore = new Datastore({ 
     projectId: process.env.GCP_PROJECT_ID,
     keyFilename: `../../${process.env.GCP_KEY_FILE_NAME}.json`
   });
+  console.log(`Datastore initialized`)
 }
 
 module.exports = datastore;
